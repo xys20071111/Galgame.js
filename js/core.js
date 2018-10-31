90170d8a6d9c167c4d4a8296f7f25088ef567142
xys20071111 <xys20071111@163.com>
2018-10-31 16:45:44 +0800

    用Pixi.js来做按钮

--- js/core.js
+++ js/core.js
@@ -1,7 +1,12 @@
 function setupSplendUI(img){
-    PIXI.loader.add(img).load(setup);
+    PIXI.loader.add(img).add(["assest/button/start/start.png",
+    "assest/button/start/load.png"]).load(setup);
     function setup() {
-        SplendUI.background = new PIXI.Sprite(PIXI.loader.resources[img].texture);
-        app.stage.addChild(SplendUI.background);
+        GameUI.background = new PIXI.Sprite(PIXI.loader.resources[img].texture);
+        app.stage.addChild(GameUI.background);
+        SplendUI.startButton = new PIXI.Sprite(PIXI.loader.resources["assest/button/start/start.png"].texture);
+        SplendUI.loadButton = new PIXI.Sprite(PIXI.loader.resources["assest/button/start/load.png"].texture);
+        SplendUI.startButton.interactive = true;
+        SplendUI.startButton.on('tap', (event) => {});
     }
 }
--- js/system.js
+++ js/system.js
@@ -1,13 +1,13 @@
 const app = new PIXI.Application({height:600,width:800});
 document.body.appendChild(app.view);
-const SplendUI ={
-    background: null,
-    bgm : null,
-};
 const GameUI = {
     bgm : null,
     background : null,
     name : null,
     text : null
 }
+var SplendUI={
+    startButton : null,
+    loadButton :null
+};
 
