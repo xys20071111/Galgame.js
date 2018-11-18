4ea55e8f009946ea658090b58b16b94ca4e4aed6
信 <xys20071111@163.com>
2018-11-18 10:18:37 +0800

    更新文件 core.js 按扭明天画

--- js/core.js
+++ js/core.js
@@ -1,11 +1,11 @@
 function setupSplendUI(img){
-    PIXI.loader.add(img).add(["assest/button/start/start.png",
-    "assest/button/start/load.png"]).load(setup);
+    PIXI.loader.add(img).add(["./assest/button/start/start.png",
+    "./assest/button/start/load.png"]).load(setup);
     function setup() {
         GameUI.background = new PIXI.Sprite(PIXI.loader.resources[img].texture);
         app.stage.addChild(GameUI.background);
-        SplendUI.startButton = new PIXI.Sprite(PIXI.loader.resources["assest/button/start/start.png"].texture);
-        SplendUI.loadButton = new PIXI.Sprite(PIXI.loader.resources["assest/button/start/load.png"].texture);
+        SplendUI.startButton = new PIXI.Sprite(PIXI.loader.resources["./assest/button/start/start.png"].texture);
+        SplendUI.loadButton = new PIXI.Sprite(PIXI.loader.resources["./assest/button/start/load.png"].texture);
         SplendUI.startButton.interactive = true;
         SplendUI.startButton.on('tap', (event) => {});
     }
