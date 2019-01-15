75fad5e243f2c4a17139171090fd7c37de140ea3
xys20071111 <xys20071111@163.com>
2019-01-15 20:50:21 +0800

    创建对话区域

--- js/screen/SplashUI.js
+++ js/screen/SplashUI.js
@@ -1,7 +1,5 @@
-const SplashUIResources=['asstes/data/bgimage/title.jpg',
-                          'asstes/data/image/button_base_large.jpg'
-                        ];
+"use strict"
 const SplashUI = {};
 PIXI.loader.add(SplashUIResources).load(()=>{
-  
+
 });
--- js/system.js
+++ js/system.js
@@ -12,3 +12,16 @@ const UI = {
 const buttonBar = new PIXI.Container();
 const splashUIButton = new PIXI.Container();
 const textBar = new PIXI.Container();
+const talkBar = new PIXI.Container();
+
+const nameDisplayed = new PIXI.Text();
+const textInTalk = new PIXI.Text();
+const text = new PIXI.text();
+
+const talkBarFrame = null;
+
+talkBar.addChild(nameDisplayed);
+talkBar.addChild(textInTalk);
+talkbar.addChild(talkBarFrame);
+
+textBar.addChild(text);
