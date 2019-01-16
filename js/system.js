65d1ccfe8cfec8dde471ba986af55885e16f7544
xys20071111 <xys20071111@163.com>
2019-01-16 13:43:02 +0800

    创建UI组件

--- js/system.js
+++ js/system.js
@@ -1,14 +1,9 @@
+"use strict"
 const app = new PIXI.Application({height:600,width:800});
 document.body.appendChild(app.view);
 const UI = {
-  background:new PIXI.Sprite,
-  set setBackground(texture){
-    this.background.texture = texture;
-  },
-  showSplashUI:function(){
-    UI.background.texture = SplashUI.background;
-  }
 };
+
 const buttonBar = new PIXI.Container();
 const splashUIButton = new PIXI.Container();
 const textBar = new PIXI.Container();
@@ -18,10 +13,14 @@ const nameDisplayed = new PIXI.Text();
 const textInTalk = new PIXI.Text();
 const text = new PIXI.text();
 
-const talkBarFrame = null;
-
+var talkBarFrame = null;
+var background = null;
 talkBar.addChild(nameDisplayed);
 talkBar.addChild(textInTalk);
 talkbar.addChild(talkBarFrame);
-
+//预加载素材并初始化背景
 textBar.addChild(text);
+PIXI.loader.add(resources).load(()=>{
+  talkBarFrame = new PIXI.Sprite(PIXI.loader.resources['asstes/data/image/frame_talk.jpg'].texture);
+  background = new PIXI.Sprite();
+});
