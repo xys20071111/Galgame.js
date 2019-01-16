288fb661453e6e3adf622b9b8207072b483e6cda
xys20071111 <xys20071111@163.com>
2019-01-16 20:46:59 +0800

    添加立绘组件

--- js/system.js
+++ js/system.js
@@ -6,15 +6,17 @@ const buttonBar = new PIXI.Container();
 const splashUIButton = new PIXI.Container();
 const textBar = new PIXI.Container();
 const talkBar = new PIXI.Container();
+const background = new PIXI.Sprite();
+const frontImage = new PIXI.Sprite();
 //UI组件成员
 const nameDisplayed = new PIXI.Text("",{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff});
 const textInTalk = new PIXI.Text("",{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff});
 const textInTextBar = new PIXI.Text("",{fontFamily : 'Arial', fontSize: 24, fill : 0xffffff});
 const talkBarFrame = new PIXI.Container();
-const nameBar = null;
-const background = new PIXI.Sprite();
+
 //初始化UI组件
 app.stage.addChild(background);
+app.stage.addChild(frontImage);
 talkBar.addChild(nameDisplayed);
 talkBar.addChild(textInTalk);
 talkBar.addChild(talkBarFrame);
