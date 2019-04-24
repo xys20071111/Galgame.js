a1d7d5135139253d480dc3df4fedac3c76f89574
xys20071111 <xys20071111@163.com>
2019-04-24 08:28:35 +0000

    修改sceneUtils.js，更改了scene的储存方式 新建error.js，定义了Error这一个类

--- js/SceneUtils.js
+++ js/SceneUtils.js
@@ -1,6 +1,7 @@
 const scene = {};
 scene.model = {
   name:null,
+  bgm:null,
   background:null,
   frontImage:null,
   characterName:null,
@@ -9,7 +10,7 @@ scene.model = {
   codeToRun:null,
 }
 scene.list = [];
-scene.create = (n,bg,fi,ntd,dtd,ns,ctr)=>{
+scene.create = (n,bg,fi,ntd,dtd,ns,ctr,bgm)=>{
   let newScene = Object.create(sceneModel);
   newScene.name = n
   newScene.background = bg;
@@ -18,27 +19,50 @@ scene.create = (n,bg,fi,ntd,dtd,ns,ctr)=>{
   newScene.dialogToDisplay = dtd;
   newScene.nextScene = ns;
   newScene.codeToRun = ctr;
+  newScene.bgm = bgm;
   scene.list.push(newScene);
 }
 scene.use = (arg)=>{
-  for(let i = 0;i <= scene.list.length;i++){
-   if(scene.list[i] == arg){} 
-  }
-  gameStatus.nowScene = target;
-  UI.talk(target.nameToDisplay,target.dialogToDisplay);
-  if(target.background != 'same'){
-      UI.setBackground(target.background);
-  }
-  if(target.frontImage != 'same'){
-    if(target.frontImage == 'none'){
-      UI.hide(frontImage);
+    if(typeof arg === 'string'){
+        for(let i = 0;i <= scene.list.length;i++){
+            if(scene.list[i] === arg){
+                gameStatus.nowScene = arg;
+                UI.talk(scene.list[i].characterName,scene.list[i].dialogToDisplay);
+                UI.setBackground(scene.list[i].background);
+                UI.setFrontImage(scene.list[i].frontImage);
+                if(scene.list[i].codeToRun){
+                    scene.list[i].codeToRun();
+                }
+                return;
+            } 
+        }
+    }else if(typeof arg === 'number'){
+        let i = arg;
+        UI.talk(scene.list[i].characterName,scene.list[i].dialogToDisplay);
+        UI.setBackground(scene.list[i].background);
+        UI.setFrontImage(scene.list[i].frontImage);
+        if(scene.list[i].codeToRun){
+            scene.list[i].codeToRun();
+        }
     }else{
-      UI.show(frontImage);
-        UI.setFrontImage(target.frontImage);
+        throw new Error("Wrong type of arg.It must be a number or string.");
     }
-  }
-  UI.setNextScene(target.nextScene);
-  if(target.codeToRun){
-    target.codeToRun();
-  }
+  
+//  gameStatus.nowScene = arg;
+//  UI.talk(target.nameToDisplay,target.dialogToDisplay);
+//  if(target.background != 'same'){
+//      UI.setBackground(target.background);
+//  }
+//  if(target.frontImage != 'same'){
+//    if(target.frontImage == 'none'){
+//      UI.hide(frontImage);
+//    }else{
+//      UI.show(frontImage);
+//        UI.setFrontImage(target.frontImage);
+//    }
+//  }
+//  UI.setNextScene(target.nextScene);
+//  if(target.codeToRun){
+//    target.codeToRun();
+//  }
 }
---
+++ js/error.js
@@ -0,0 +1,8 @@
+class Error(){
+    constructor(err){
+        this.errMsg = err;
+    }
+    getErr(){
+        return this.errMsg;
+    }
+}
