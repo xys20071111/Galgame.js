a305f805b220f8912b4590904c0c70848016e45c
xys20071111 <xys20071111@163.com>
2019-08-07 12:42:18 +0000

    添加nw.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const fs = require('fs'); 
 const express = require('express');
 const app = express();
-app.use('/index.html',express.static('./index.html'));
+app.use('/i',express.static('./index.html'));
 app.use('/js',express.static('./js'));
 app.use('/asstes',express.static('./asstes'));
 app.listen(8080)
