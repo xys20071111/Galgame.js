8ded1f0f35fa30e32d7e28af93ad1e8661156aab
Tom Xue <Xys20071111@163.com>
2020-02-11 16:03:16 +0800

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -1,14 +1,15 @@
 "use strict"
-const UI = {};
 //基本UI组件
-const buttonBar = new PIXI.Container();
-const splashUIButton = new PIXI.Container();
-const talkBar = new PIXI.Container();
-UI.hide=(target)=>{
+import * as PIXI from "pixi.js"
+import {Stage,Loader} from "./app"
+export const buttonBar = new PIXI.Container();
+export const splashUIButton = new PIXI.Container();
+export const dialogBar = new PIXI.Container();
+export const hide=(target)=>{
 	target.alpha = 0;
 	target.visible=false;
 };
-UI.show = (target)=>{
+export const show = (target)=>{
 	target.alpha = 100;
 	target.visible = true;
 };
@@ -30,7 +31,7 @@ UI.setBackground = (texture) => {
 UI.setFrontImage = (texture) => {
   frontImage.texture = PIXI.loader.resources[texture].texture;
 }*/
-UI.display={
+export default const display={
 	//UI组件成员
 	frontImage : new PIXI.Sprite(),
 	backImage : new PIXI.Sprite(),
@@ -39,72 +40,72 @@ UI.display={
 	talkBarFrame : new PIXI.Sprite(),
 	startButton : new PIXI.Sprite(),
 	loadButton : new PIXI.Container(),
-	FrontImage : function(value){UI.display.frontImage.texture = PIXI.loader.resources[value].texture},
-	BackImage : function(value){UI.display.backImage.texture = PIXI.loader.resources[value].texture},
-	Dialog : function(value){UI.display.name.text = value.name;UI.display.dialog.text = value.dialog}
+	FrontImage : function(value){display.frontImage.texture = PIXI.loader.resources[value].texture},
+	BackImage : function(value){display.backImage.texture = PIXI.loader.resources[value].texture},
+	Dialog : function(value){display.name.text = value.name;UI.display.dialog.text = value.dialog}
 
 }
-UI.setStartUI = (startButtonTexture,loadButtonTexture,targetScene) => {
+export const setStartUI = (startButtonTexture,loadButtonTexture,targetScene) => {
 	console.log('Init startUI');
 	//设置开始按钮位置
-	UI.display.startButton.x = 300; 
-	UI.display.startButton.y = 400;
+	display.startButton.x = 300; 
+	display.startButton.y = 400;
 	//设置开始按钮材质
-	UI.display.startButton.texture = PIXI.loader.resources[startButtonTexture].texture;
-	UI.display.loadButton.texture = PIXI.loader.resources[loadButtonTexture].texture;
-	UI.display.startButton.interactive = true;
-	UI.display.startButton.on(click, (event) => {
+	display.startButton.texture = PIXI.loader.resources[startButtonTexture].texture;
+	display.loadButton.texture = PIXI.loader.resources[loadButtonTexture].texture;
+	display.startButton.interactive = true;
+	display.startButton.on(click, (event) => {
 		//UI.setupGameUI(底下那个框的材质,第一个场景的名称)
 		console.log('Start game');
-		UI.setupGameUI('asstes/data/image/frame.png',targetScene);
+		setupGameUI('asstes/data/image/frame.png',targetScene);
 	});
-	app.stage.addChild(splashUIButton);
+	Stage.addChild(splashUIButton);
 }
-UI.setupGameUI = (framePic,startScene) => {
+export const setupGameUI = (framePic,startScene) => {
 	UI.hide(splashUIButton);
 	//组建游戏界面
-	app.stage.addChild(UI.display.frontImage);
-	app.stage.addChild(talkBar);
-	app.stage.addChild(buttonBar);
-	UI.display.frontImage.x = 100;
-	UI.display.frontImage.y = 45;
-	UI.display.frontImage.width = 500;
-	UI.display.frontImage.height = 600;
-	UI.display.talkBarFrame.texture = PIXI.loader.resources[framePic].texture;
+	Stage.addChild(UI.display.frontImage);
+	Stage.addChild(talkBar);
+	Stage.addChild(buttonBar);
+	display.frontImage.x = 100;
+	display.frontImage.y = 45;
+	display.frontImage.width = 500;
+	display.frontImage.height = 600;
+	display.talkBarFrame.texture = PIXI.loader.resources[framePic].texture;
 	//设置组件位置
-	UI.display.name.x = 100;
-	UI.display.name.y = 25;
-	UI.display.dialog.x = 30;
-	UI.display.dialog.y = 65;
+	display.name.x = 100;
+	display.name.y = 25;
+	display.dialog.x = 30;
+	display.dialog.y = 65;
 	talkBar.y = 310;
 	scene.use(startScene);
 }
-UI.setNextScene = (nextScene)=>{
+export const setNextScene = (nextScene)=>{
 	talkBar.on(click,()=>{
 		scene.use(nextScene);
 	});
-	UI.display.background.on(click,()=>{
+	display.background.on(click,()=>{
 		scene.use(nextScene);
 	});
-	UI.display.frontImage.on(click,()=>{
+	display.frontImage.on(click,()=>{
 		scene.use(nextScene);
 	});
 }
-UI.createButton = (bg,x,y,onClick)=>{
+export const createButton = (bg,x,y,onClick)=>{
 	let newButton = new PIXI.Sprite(PIXI.loader.resources[bg].texture);
 	newButton.x = x;
 	newButton.y = y;
 	newButton.interactive = true;
 	newButton.on(click,onClick);
-	app.stage.addChild(newButton);
+	Stage.addChild(newButton);
 	return newButton;
 };
 //初始化UI组件
-splashUIButton.addChild(UI.display.startButton);
-splashUIButton.addChild(UI.display.loadButton);
-talkBar.addChild(UI.display.nameDisplayed);
-talkBar.addChild(UI.display.textInTalk);
-talkBar.addChild(UI.display.talkBarFrame);
+splashUIButton.addChild(display.startButton);
+splashUIButton.addChild(display.loadButton);
+talkBar.addChild(display.nameDisplayed);
+talkBar.addChild(display.textInTalk);
+talkBar.addChild(display.talkBarFrame);
 talkBar.interactive = true;
-UI.display.backImage.interactive = true;
-UI.dispaly.frontImage.interactive = true;
+display.backImage.interactive = true;
+dispaly.frontImage.interactive = true;
